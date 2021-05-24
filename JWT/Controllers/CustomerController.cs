c96ad831307c2c5f7163ed86b68059bdb4a08147
Melissa Govender <MelissaG@hollywoodbets.net>
2021-05-24 13:23:45 +0200

    Add project files.

---
+++ JWT/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        [HttpGet,Authorize]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "John Doe", "Jane Doe"}; 
+        }
+    }
+}
